e48fe1cd1b194bffee4e8f84da0552ba7736e61e
mingadinga <minpearl0826@gmail.com>
2023-03-10 09:09:25 +0900

    1. 람다 표현식과 함수형 인터페이스 소개

---
+++ src/main/java/me/whiteship/Foo.java
@@ -0,0 +1,25 @@
+package me.whiteship;
+
+public class Foo {
+    public static void main(String[] args) {
+        // 익명 내부 클래스
+//        RunSomething = new RunSomething() {
+//            @Override
+//            public void doIt() {
+//                System.out.println("Hello");
+//            }
+//        };
+
+        // 람다 표현식
+        // 자바는 객체지향언어이므로 람다 표현식을 특수한 형태의 오브젝트로 다룬다
+        // 변수에 할당하고 메소드의 파라미터로 전달하거나 리턴할 수 있다 : 고차 함수
+        // 동일한 파라미터에 대해 동일한 값을 반환한다. 상태와 사이드 이펙트가 없다. 함수 밖의 값을 사용거나 변경하지 않는다. : 순수 함수
+        RunSomething runSomething = (number) -> {
+            return number + 10;
+        };
+        System.out.println(runSomething.doIt(5)); // 15
+        System.out.println(runSomething.doIt(5)); // 15
+        System.out.println(runSomething.doIt(5)); // 15
+
+    }
+}
---
+++ src/main/java/me/whiteship/RunSomething.java
@@ -0,0 +1,21 @@
+package me.whiteship;
+
+// 함수형 인터페이스
+// 추상 메소드가 딱 하나 존재한다
+// 추상 메소드 외에 다른 형태의 메소드를 정의할 수 있다
+// 자바가 제공하는 애노테이션을 추가하면 추상 메소드를 두개 이상 선언했을 때 컴파일 오류가 난다
+@FunctionalInterface
+public interface RunSomething {
+    // 추상 메소드가 하나 있는게 함수형 인터페이스
+    int doIt(int number);
+
+    // 함수형 인터페이스 안에 static 메소드를 정의할 수 있다
+    static void printName(){
+        System.out.println("Hwi");
+    }
+
+    // 함수형 인터페이스 안에 default 메소드를 정의할 수 있다
+    default void printAge() {
+        System.out.println("24");
+    }
+}
