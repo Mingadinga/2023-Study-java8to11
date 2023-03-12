2112d5d0a0d78311adae431b052abd07fbfc8937
mingadinga <minpearl0826@gmail.com>
2023-03-13 07:49:11 +0900

    4. 메소드 레퍼런스

---
+++ src/main/java/me/whiteship/functionalinterface/MethodReference.java
@@ -0,0 +1,28 @@
+package me.whiteship.functionalinterface;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class MethodReference {
+    public static void main(String[] args) {
+        Supplier<Greeting> defaultConstructor = Greeting::new;
+        Greeting greeting1 = defaultConstructor.get();
+
+        Function<String, Greeting> nameConstructor = Greeting::new;
+        Greeting greeting2 = nameConstructor.apply("minpearl");
+
+        Function<String, String> staticHi = Greeting::hi;
+        System.out.println(staticHi.apply("Min"));
+
+        Greeting greeting3 = defaultConstructor.get();
+        Function<String, String> hello = greeting3::hello;
+        System.out.println(hello.apply("min"));
+
+        String[] names = {"min", "hwi", "seeun"};
+        Arrays.sort(names, (o1, o2) -> o1.compareToIgnoreCase(o2)); // 람다식
+        Arrays.sort(names, String::compareToIgnoreCase); // 메소드 레퍼런스
+
+    }
+
+}
